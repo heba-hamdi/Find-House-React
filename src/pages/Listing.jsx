dd6c165c65dc5739a3a63eb0455d12df917ee4b7
Heba Hamdi <dr.hebaelgamil@gmail.com>
2023-02-20 14:32:18 +0200

    add listing details

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,31 +1,36 @@
-import { doc, getDoc } from 'firebase/firestore'
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import { useParams } from 'react-router'
-import Loader from '../components/loader/Loader'
-import { db } from '../firebase'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation, Pagination, Autoplay, EffectFade } from 'swiper';
-import 'swiper/css/bundle';
+import { doc, getDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import Loader from "../components/loader/Loader";
+import { db } from "../firebase";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Autoplay,
+  EffectFade,
+} from "swiper";
+import "swiper/css/bundle";
 
 const Listing = () => {
-  const params = useParams()
-  const [listing, setListing] = useState(null)
-  const [loading, setLoading] = useState(true)
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
   SwiperCore.use([Navigation, Pagination, Autoplay]);
- 
+
   useEffect(() => {
     const fetchDoc = async () => {
-      const docRef = doc(db, "listings", params.listingId)
-      const docSnap = await getDoc(docRef)
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
-        setListing(docSnap.data())
-        setLoading(false)
-        console.log(docSnap.data())
+        setListing(docSnap.data());
+        setLoading(false);
+        console.log(docSnap.data());
       }
-    }
+    };
     fetchDoc();
-  }, [params.listingId])
+  }, [params.listingId]);
   if (loading) {
     return <Loader />;
   }
@@ -41,25 +46,27 @@ const Listing = () => {
         pagination={{ type: "progressbar" }}
         autoplay={3000}
         effect={"fade"}
-       loop     
+        loop
       >
         {listing.imgUrls.map((item, index) => {
-          return <SwiperSlide>
-            <div className='background relative' style={{
-              background:`url('${item}')`,
-              height:'600px',
-              backgroundRepeat:'no-repeat',
-              backgroundSize:'cover',
-              backgroundPosition:'center' ,             
-                 
-            }}>
-              
-            </div>
-          </SwiperSlide>;
+          return (
+            <SwiperSlide>
+              <div
+                className="background relative"
+                style={{
+                  background: `url('${item}')`,
+                  height: "600px",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              ></div>
+            </SwiperSlide>
+          );
         })}
       </Swiper>
     </main>
-  )
-}
+  );
+};
 
-export default Listing
+export default Listing;
