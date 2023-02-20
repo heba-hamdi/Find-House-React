d2253ead157b4bdf732a4b008f22c146a2c9a713
Heba Hamdi <dr.hebaelgamil@gmail.com>
2023-02-20 14:33:32 +0200

    add more listing details

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -12,6 +12,7 @@ import SwiperCore, {
   EffectFade,
 } from "swiper";
 import "swiper/css/bundle";
+import { FaBed, FaBath, FaChair, FaParking } from "react-icons/fa";
 
 const Listing = () => {
   const params = useParams();
@@ -52,19 +53,88 @@ const Listing = () => {
           return (
             <SwiperSlide>
               <div
-                className="background relative"
+                className=" w-  background relative bg-cover"
                 style={{
                   background: `url('${item}')`,
                   height: "600px",
                   backgroundRepeat: "no-repeat",
-                  backgroundSize: "cover",
                   backgroundPosition: "center",
+                  backgroundSize: "cover",
                 }}
               ></div>
             </SwiperSlide>
           );
         })}
       </Swiper>
+      <section>
+        <div className="pt-12 max-w-6xl m-auto">
+          <div className="flex justify-between items-center">
+            <div>
+              <h2 className="font-bold text-3xl mb-3">{listing.name}</h2>
+              <p className="text-gray-500 text-sm">{listing.address}</p>
+            </div>
+            <div>
+              <div className="flex items-center justify-center">
+                <p className="font-bold text-3xl ">
+                  $
+                  {listing.regularPrice
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                </p>
+                {listing.type === "rent" && (
+                  <span className="text-lg">/mo</span>
+                )}
+              </div>
+              {listing.offer && (
+                <div className=" flex space-x-3 items-center mt-2">
+                  <p className="bg-red-500 text-white p-2 rounded-lg">
+                    For {listing.type}
+                  </p>
+                  <p className=" bg-[#006A70] text-white p-2 rounded-lg">
+                    $
+                    {(+listing.regularPrice - +listing.discountedPrice)
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+                    discount
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className="bg-white mt-12 rounded-md">
+            <div className="m-6">
+              <ul className="flex space-x-4 py-10">
+                <li className="bg-gray-100 py-2 px-4 rounded-md text-sm hover:text-red-500 cursor-pointer flex items-center">
+                  <FaBed className="mr-2" />
+                  {listing.bedrooms > 1
+                    ? `${listing.bedrooms} Beds`
+                    : `${listing.bedrooms} Bed`}
+                </li>
+                <li className="bg-gray-100 py-2 px-4 rounded-md text-sm hover:text-red-500 cursor-pointer flex items-center">
+                  <FaBath className="mr-2" />
+                  {listing.bathrooms > 1
+                    ? `${listing.bathrooms} Baths`
+                    : `${listing.bathrooms} Bath`}
+                </li>
+                <li className="bg-gray-100 py-2 px-4 rounded-md text-sm hover:text-red-500 cursor-pointer flex items-center">
+                  <FaChair className="mr-2" />
+                  {listing.furnished ? "Furnished" : "Not Furnished"}
+                </li>
+                <li className="bg-gray-100 py-2 px-4 rounded-md text-sm hover:text-red-500 cursor-pointer flex items-center">
+                  <FaParking className="mr-2" />
+                  {listing.parking ? "Parking" : "No Parking"}
+                </li>
+              </ul>
+
+              <h2 className="font-bold text-lg mb-3 text-gray-600">
+                Description
+              </h2>
+              <p className="text-gray-600">{listing.description}</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
